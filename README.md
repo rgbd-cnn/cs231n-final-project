fcd4598e3ccd66ee6d6977225083a43454ef2dba
Bohan Wu <bhwu@mobvoi.com>
2017-04-30 00:13:42 +0800

    Completed python script for nyu_depth_v2 dataset

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### `cs231n-final-project`
-##### Authors: Jakub Dworakowski (jakub@stanford.edu), Tyler Mau(tylermau@stanford.edu), Bohan Wu (bohanwu@stanford.edu)
+##### Authors: Jakub Dworakowski (jakub@stanford.edu), Tyler Mau (tylermau@stanford.edu), Bohan Wu (bohanwu@stanford.edu)
 
 #### `Logistics:`
 1. Remember to create your own dev branch before merging into master remotely: e.g. bohanwu-dev
@@ -8,8 +8,14 @@
 1. [UCL Laundry list](http://www0.cs.ucl.ac.uk/staff/M.Firman/RGBDdatasets/)
 
 #### `Datasets:`
-1. [NYU RGB-D Dataset](http://horatio.cs.nyu.edu/mit/silberman/nyu_depth_v2/nyu_depth_v2_labeled.mat)
-* Currently encountering issues extracting label information. The images are fine. In this case, we might need to use other datasets.
+1. [NYU RGB-D Dataset](http://cs.nyu.edu/~silberman/datasets/nyu_depth_v2.html)
+* Dataset info
+    * 1449 (640 x 480) images containing 894 object types: books, bottles, etc.
+    * depth info included
+* Image extraction python script in `data/nyu_depth_v2/load_mat.py`
+    * `save_original_images_to_disk()` saves all 1449 images in `raw-images`
+    * `save_single_object_images_to_disk()` creates 894 subfolders under `single-object-images` and put all images containing a specific object type in one of these 894 subfolders
+    * more functions to come based on necesscity
 2. [U-WASH RGB-D Dataset](http://rgbd-dataset.cs.washington.edu/dataset/rgbd-scenes-v2/)
 * Analyzing currently.
 3. [Princeton the SUN RGB-D Dataset](http://rgbd.cs.princeton.edu/challenge.html)
--- data/nyu_depth_v2/load_mat.py
+++ data/nyu_depth_v2/load_mat.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import sys
 
 import h5py
@@ -14,31 +15,54 @@ def format_pixel_data(pixel_array, h, w):
     return list
 
 
-def save_images_to_disk(data_file_dir):
+def save_original_images_to_disk(data_file_dir):
     f = h5py.File(data_file_dir, 'r')
     images = np.array(f.get("images"))
+    save_images_to_disk(images, "raw-images")
+
+
+def save_images_to_disk(images, root_dir):
     W, H = np.ma.size(images, axis=2), np.ma.size(images, axis=3)
+    print np.ma.size(images, axis=0)
     for i in range(np.ma.size(images, axis=0)):
         print i
         im = Image.new('RGB', (W, H))
         im.putdata(format_pixel_data(images[i, :, :, :], H, W))
-        im.save('raw-images/%s.png' % i)
+        im.save(os.path.join(root_dir, "%s.png" % i))
 
 
-def load_data(data_file_dir):
+def save_single_object_images_to_disk(data_file_dir):
     f = h5py.File(data_file_dir, 'r')
+    labels = ["".join([chr(j) for j in f[i].value]) for i in
+              f.get("names").value[0]]
+    pixel_labels = np.array(f.get("labels"))
     images = np.array(f.get("images"))
+    num_images = images.shape[0]
+    root_dir = "single-object-images"
+    for i in range(len(labels)):
+        label = labels[i]
+        label_sub_dir = os.path.join(os.path.expanduser(root_dir), label)
+        if not os.path.exists(label_sub_dir):
+            os.makedirs(label_sub_dir)
+        images_to_save = []
+        for j in range(num_images):
+            image = images[j]
+            pixel_label = pixel_labels[j]
+            if np.sum(pixel_label == (i + 1)) > 0:
+                images_to_save.append(image)
+        save_images_to_disk(np.array(images_to_save), label_sub_dir)
 
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--data_file_dir', type=str,
-                        help='Directory for the matlab data file.',
+                        help='Directory of the matlab data file.',
                         default='./nyu_depth_v2_labeled.mat')
     return parser.parse_args(argv)
 
 
 if __name__ == '__main__':
     dir = parse_arguments(sys.argv[1:]).data_file_dir
-    load_data(dir)
+    # save_original_images_to_disk(dir)
+    # save_single_object_images_to_disk(dir)
