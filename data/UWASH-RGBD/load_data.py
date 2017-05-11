1c25e902ed3ea2271d296e7e513ca2a620185e6e
Bohan Wu <bhwu@mobvoi.com>
2017-05-11 11:49:54 +0800

    completed UWASH dataset processing script

--- data/UWASH-RGBD/load_data.py
+++ data/UWASH-RGBD/load_data.py
@@ -1,6 +1,9 @@
-import numpy as np
-import os
 import argparse
+import json
+import os
+import sys
+
+import numpy as np
 from PIL import Image
 
 
@@ -12,12 +15,15 @@ def parse_arguments(argv):
                         default='./rgbd-dataset')
     parser.add_argument('--target_img_height', type=int,
                         help='Target image height with which to resize images',
-                        default=256)
+                        default=64)
     parser.add_argument('--target_img_width', type=int,
                         help='Target image width with which to resize images',
-                        default=256)
+                        default=64)
     parser.add_argument('--save_resized_images_to_disk', type=bool,
                         help='whether to save resized images to disk',
+                        default=False)
+    parser.add_argument('--load_from_resized_images', type=bool,
+                        help='whether to load resized images saved previously',
                         default=False)
     return parser.parse_args(argv)
 
@@ -30,17 +36,16 @@ def is_rgb_file(file):
 def read_and_resize_image(file_dir, depth_dir, height, width):
     desired_dimension = (width, height)
     im = Image.open(file_dir)
-    out = im.resize(desired_dimension, Image.ANTIALIAS)
-    rgb = np.array(out)
+    out_rgb = im.resize(desired_dimension, Image.ANTIALIAS)
+    rgb = np.array(out_rgb)
 
     im = Image.open(depth_dir)
-    out = im.resize(desired_dimension, Image.ANTIALIAS)
-    depth = np.array(out)
+    out_depth = im.resize(desired_dimension, Image.ANTIALIAS)
+    depth = np.reshape(np.array(out_depth), (height, width, 1))
+    return np.concatenate((rgb, depth), axis=2), rgb, depth
 
-    return np.concatenate((rgb, depth), axis=2)
 
-
-def save_file(data_dir, object, folder, rgb_file, depth_file, image):
+def save_file(data_dir, object, folder, rgb_file, depth_file, rgb, depth):
     resized_object_dir = os.path.join(data_dir, object + "_resized")
     resized_folder_dir = os.path.join(resized_object_dir, folder)
     if not os.path.exists(resized_object_dir):
@@ -50,44 +55,90 @@ def save_file(data_dir, object, folder, rgb_file, depth_file, image):
     rgb_dir = os.path.join(resized_folder_dir, rgb_file)
     depth_dir = os.path.join(resized_folder_dir, depth_file)
 
-    im = Image.fromarray(np.array(image[:,:,:3]))
+    im = Image.fromarray(rgb, 'RGB')
     im.save(rgb_dir)
 
-    im = Image.fromarray(np.array(image[:,:,3]))
+    im = Image.fromarray(depth, 'RGB')
     im.save(depth_dir)
     print("Saved to disk: %s and %s" % (rgb_dir, depth_dir))
 
 
-def get_np_arrays_from_dataset(data_dir, height, width, save):
+def load_original(data_dir, height, width, save, load_resized):
     X = []
     Y = []
     for object in os.listdir(data_dir):
-        object_dir = os.path.join(data_dir, object)
-        if os.isdir(object_dir):
-            for folder in os.listdir(object_dir):
-                folder_dir = os.path.join(object_dir, folder)
-                if os.isdir(folder_dir):
-                    dirs = os.listdir(folder_dir)
-                    for file in dirs:
-                        if is_rgb_file(file):
-                            depth_file = file[:-8] + "depthcrop.png"
-                            if depth_file in dirs:
-                                file_dir = os.path.join(folder_dir, file)
-                                depth_dir = os.path.join(folder_dir, depth_file)
-                                x = read_and_resize_image(file_dir, depth_dir,
-                                                          height, width)
-                                X.append()
-                                Y.append(object)
-                                print("Loaded: %s, %s" % (file_dir, depth_dir))
-                                if save:
-                                    save_file(data_dir, object, folder, file,
-                                              depth_file, x)
+        if "_resized" not in object:
+            object_dir = os.path.join(data_dir, object)
+            if os.path.isdir(object_dir):
+                for folder in os.listdir(object_dir):
+                    folder_dir = os.path.join(object_dir, folder)
+                    if os.path.isdir(folder_dir):
+                        dirs = os.listdir(folder_dir)
+                        for file in dirs:
+                            if is_rgb_file(file):
+                                depth_file = file[:-8] + "depthcrop.png"
+                                if depth_file in dirs:
+                                    file_dir = os.path.join(folder_dir, file)
+                                    depth_dir = os.path.join(folder_dir,
+                                                             depth_file)
+                                    x, rgb, depth = read_and_resize_image(
+                                        file_dir, depth_dir, height, width)
+                                    X.append(x)
+                                    Y.append(object)
+                                    print(
+                                        "Loaded: %s, %s" % (
+                                            file_dir, depth_dir))
+                                    if save:
+                                        save_file(data_dir, object, folder,
+                                                  file, depth_file, rgb, depth)
+
+    return X, Y
 
 
+def load_resized(data_dir, height, width, save, load_resized):
+    X = []
+    Y = []
+    for object in os.listdir(data_dir):
+        label = object[:-8]
+        if "_resized" in object:
+            object_dir = os.path.join(data_dir, object)
+            if os.path.isdir(object_dir):
+                for folder in os.listdir(object_dir):
+                    folder_dir = os.path.join(object_dir, folder)
+                    if os.path.isdir(folder_dir):
+                        dirs = os.listdir(folder_dir)
+                        for file in dirs:
+                            if is_rgb_file(file):
+                                depth_file = file[:-8] + "depthcrop.png"
+                                if depth_file in dirs:
+                                    file_dir = os.path.join(folder_dir, file)
+                                    depth_dir = os.path.join(folder_dir,
+                                                             depth_file)
+                                    x, rgb, depth = read_and_resize_image(
+                                        file_dir, depth_dir, height, width)
+                                    X.append(x)
+                                    Y.append(label)
+                                    print(
+                                        "Loaded: %s, %s" % (
+                                            file_dir, depth_dir))
+    return X, Y
 
 
+def get_np_arrays_from_dataset(data_dir, height, width, save, load_resized):
+    if load_resized:
+        X, Y = load_resized(data_dir, height, width, save, load_resized)
+    else:
+        X, Y = load_original(data_dir, height, width, save, load_resized)
 
     return np.array(X), Y
+
+
+def save_X_and_Y_json_to_disk(x, y):
+    with open('data.json', 'w') as f:
+        json.dump({"X": x.tolist(), "Y": y}, f)
+
+    f.close()
+
 
 if __name__ == '__main__':
     args = parse_arguments(sys.argv[1:])
@@ -95,6 +146,7 @@ if __name__ == '__main__':
     height = args.target_img_height
     width = args.target_img_width
     save = args.save_resized_images_to_disk
-    X, Y = get_np_arrays_from_dataset(data_dir, height, width, save)
-
-
+    load_resized = args.load_from_resized_images
+    X, Y = get_np_arrays_from_dataset(data_dir, height, width, save,
+                                      load_resized)
+    save_X_and_Y_json_to_disk(X, Y)
