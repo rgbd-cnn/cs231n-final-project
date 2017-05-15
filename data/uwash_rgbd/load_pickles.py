219c02e6e68c86e104702fea9547a3e022d7551f
Bohan Wu <bhwu@mobvoi.com>
2017-05-15 21:08:50 +0800

    added test suite daemon script

--- data/uwash_rgbd/load_pickles.py
+++ data/uwash_rgbd/load_pickles.py
@@ -55,9 +55,11 @@ def package_data(X_train, y_train, X_test_val, y_test_val, depth):
         X_train = X_train[:, :, :, 0:3]
         X_test_val = X_test_val[:, :, :, 0:3]
 
-    # Normalize Depth Data (0 to 255)
-    X_train[:, :, :, 3] *= (255.0 / np.max(X_train[:, :, :, 3], axis=(1,2)))[:, np.newaxis, np.newaxis]
-    X_test_val[:, :, :, 3] *= (255.0 / np.max(X_test_val[:, :, :, 3], axis=(1,2)))[:, np.newaxis, np.newaxis]
+    else:
+        # Normalize Depth Data (0 to 255)
+        print("Normalize Depth Data (0 to 255)")
+        X_train[:, :, :, 3] *= (255.0 / np.max(X_train[:, :, :, 3], axis=(1,2)))[:, np.newaxis, np.newaxis]
+        X_test_val[:, :, :, 3] *= (255.0 / np.max(X_test_val[:, :, :, 3], axis=(1,2)))[:, np.newaxis, np.newaxis]
 
     # Shuffle Training Data
     print("Shuffling training data...")
