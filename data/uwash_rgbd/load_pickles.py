7e7df0843129385e2b22fa4e35f21a1da4256da0
Tyler Mau <tyler_mau@apple.com>
2017-05-15 00:25:05 -0700

    Fixed normalization bug

--- data/uwash_rgbd/load_pickles.py
+++ data/uwash_rgbd/load_pickles.py
@@ -57,6 +57,7 @@ def package_data(X_train, y_train, X_test_val, y_test_val, depth):
 
     # Normalize Depth Data (0 to 255)
     X_train[:, :, :, 3] *= (255.0 / np.max(X_train[:, :, :, 3], axis=(1,2)))[:, np.newaxis, np.newaxis]
+    X_test_val[:, :, :, 3] *= (255.0 / np.max(X_test_val[:, :, :, 3], axis=(1,2)))[:, np.newaxis, np.newaxis]
 
     # Shuffle Training Data
     print("Shuffling training data...")
