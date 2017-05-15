d92dc45967951cf464ac4b7605aba63a6cbac3a2
Bohan Wu <bhwu@mobvoi.com>
2017-05-16 00:49:22 +0800

    trying unnormalized depth dta

--- data/uwash_rgbd/load_pickles.py
+++ data/uwash_rgbd/load_pickles.py
@@ -56,10 +56,11 @@ def package_data(X_train, y_train, X_test_val, y_test_val, depth):
         X_test_val = X_test_val[:, :, :, 0:3]
 
     else:
+        pass
         # Normalize Depth Data (0 to 255)
-        print("Normalize Depth Data (0 to 255)")
-        X_train[:, :, :, 3] *= (255.0 / np.max(X_train[:, :, :, 3], axis=(1,2)))[:, np.newaxis, np.newaxis]
-        X_test_val[:, :, :, 3] *= (255.0 / np.max(X_test_val[:, :, :, 3], axis=(1,2)))[:, np.newaxis, np.newaxis]
+        # print("Normalize Depth Data (0 to 255)")
+        # X_train[:, :, :, 3] *= (255.0 / np.max(X_train[:, :, :, 3], axis=(1,2)))[:, np.newaxis, np.newaxis]
+        # X_test_val[:, :, :, 3] *= (255.0 / np.max(X_test_val[:, :, :, 3], axis=(1,2)))[:, np.newaxis, np.newaxis]
 
     train_size = y_train.shape[0]
     test_val_size = y_test_val.shape[0]
