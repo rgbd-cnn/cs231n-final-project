a2c5e307d6698250cb40118cdac2e7077bb13098
Bohan Wu <bhwu@mobvoi.com>
2017-06-04 11:47:14 +0800

    viz

--- data/visualize_depth_map.py
+++ data/visualize_depth_map.py
@@ -2,7 +2,7 @@ import json
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-
+from PIL import Image
 
 def visualize(f):
     with open(f, 'r') as fp:
@@ -12,10 +12,12 @@ def visualize(f):
         plt.title(yinp['label'][i])
         map = yinp['data'][i]
         ii = plt.imshow(np.array(map)[:, :, 0], interpolation='nearest')
+        plt.colorbar()
         fig = plt.figure(2)
         plt.title(yinp['label'][i])
         map = yinp['X'][i]
-        ii = plt.imshow(np.array(map), interpolation='nearest')
+        ii = plt.imshow(np.array(map).astype('uint8'))
+        plt.colorbar()
         plt.show()
 
 dir = './depth_maps'
