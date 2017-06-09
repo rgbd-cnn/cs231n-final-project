daff35daf89a032896782f66b577cab0e0380517
Bohan Wu <bhwu@mobvoi.com>
2017-06-09 11:42:36 +0800

    test

--- tests/test_depth_enhanced_cnn.py
+++ tests/test_depth_enhanced_cnn.py
@@ -163,7 +163,7 @@ def run_depth_enhanced_cnn_test(data, num_classes, device, recover, ckpt_path,
                     "rgb": RGB.tolist(),
                     'd': D.tolist(),
                     'dict': data['dict'],
-                    'label': data['y_val'][:128],
+                    'label': data['y_val'][:128].tolist(),
                     'image': data['X_val_unnormalized'][:128].tolist(),
                     'depth': dmap.tolist()
                 }, f)
