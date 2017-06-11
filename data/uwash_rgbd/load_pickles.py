7633c0f01f3fb8d543c717e3051ab46347c3f250
Bohan Wu <bhwu@mobvoi.com>
2017-06-11 15:28:16 +0800

    doing depth inversion

--- data/uwash_rgbd/load_pickles.py
+++ data/uwash_rgbd/load_pickles.py
@@ -82,8 +82,8 @@ def package_data(X_train, y_train, X_test_val, y_test_val, depth):
         # X_test_val[:, :, :, 3][X_test_val[:, :, :, 3] > 5000] = 5000
 
         # Invert Depth Data
-        X_train[:, :, :, 3] = 1.0 / X_train[:, :, :, 3]
-        X_test_val[:, :, :, 3] = 1.0 / X_test_val[:, :, :, 3]
+        # X_train[:, :, :, 3] = 1.0 / X_train[:, :, :, 3]
+        # X_test_val[:, :, :, 3] = 1.0 / X_test_val[:, :, :, 3]
         # X_train[:, :, :, 3][X_train[:, :, :, 3] > 5000] = 5000
         # print(np.max(X_train[:, :, :, 3]))
 
